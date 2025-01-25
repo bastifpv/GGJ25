c1f509d925066238fce6b7b73ec33920073c99ab
Bastian Reuther <bastir25@icloud.com>
2025-01-26 00:37:48 +0100

    process raus

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -17,10 +17,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_slider_update(value: bool) -> void:
 	if value:
 		AudioServer.set_bus_volume_db(fx_index, linear_to_db(fxSlider.value))
