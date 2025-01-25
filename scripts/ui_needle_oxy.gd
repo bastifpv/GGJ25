6acb219e2540b2caa8295757c39dbf44d54748f9
IreneSmober <irenesmober@gmail.com>
2025-01-25 14:36:37 +0100

    home arrow works in With UI

--- scripts/ui_needle_oxy.gd
+++ scripts/ui_needle_oxy.gd
@@ -7,6 +7,7 @@ var relative_oxy
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	$Oxy/needle_oxy.rotation_degrees = 0
+	$home/Arrow_home.rotation_degrees = 30
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,8 +19,18 @@ func _process(delta: float) -> void:
 		relative_oxy = 1
 	var degree = -90 + 180 * relative_oxy	
 	$Oxy/needle_oxy.rotation_degrees = degree
-	$Oxy/lbl_oxy.text = str(relative_oxy*100) + "%" 
-
+	$Oxy/lbl_oxy.text = str(int(relative_oxy)*100) + "%" 
+	
+	# Dome Posi
+	#print ("X: ")
+	#print($"../../Player".global_position.x)
+	#print ("Y: ")
+	#print($"../../Player".global_position.y)
+	var rel_y = $"../../Player".global_position.y - $"../../../dome".global_position.y
+	var rel_x = $"../../Player".global_position.x - $"../../../dome".global_position.x
+	var angle_to_dome = 180 - (tan(rel_y / rel_x)*180/3.1415926)
+	
+	$home/Arrow_home.rotation_degrees = angle_to_dome
 
 func receive_oxygen_base_change(val: Variant) -> void:
 	oxy = val
