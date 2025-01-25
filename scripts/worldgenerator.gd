cdb7e320b4e4829cbafc9091d932fa5afed0ebf3
Bastian Reuther <bastir25@icloud.com>
2025-01-25 19:51:05 +0100

    repeat the junk placement on both sides in renderdistance

--- scripts/worldgenerator.gd
+++ scripts/worldgenerator.gd
@@ -5,6 +5,8 @@ var chunk1 = preload("res://scenes/prefabs/chunks/chunk1.tscn")
 const RENDER_DISTANCE = 50
 var MaxRenderd = 0
 var MinRenderd = 0
+var MaxPlacedChunks = 0
+var MinPlacedChunks = 0
 var player
 const CHUNK_SIZE = 50
 
@@ -21,6 +23,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	player = get_node("../PlayerRig/Player")
 	on_move_generate_sand(player.position)
+	on_move_place_chunk(player.position)
 	pass
 
 func generate_sand_initial():
@@ -55,16 +58,34 @@ func on_move_generate_sand(player_position):
 
 func generate_initial_world():
 	#Generate left Chunk 
-	var chunk_right : Node3D = chunk1.instantiate()
-	add_child(chunk_right)
-	chunk_right.position = Vector3(0,0,0)
+	var chunk_right1 : Node3D = chunk1.instantiate()
+	add_child(chunk_right1)
+	chunk_right1.position = Vector3(0,0,0)
 	#Generate right Chunk
-	var chunk_left : Node3D = chunk1.instantiate()
-	add_child(chunk_left)
-	chunk_left.position = Vector3(0-CHUNK_SIZE,0,0)
+	var chunk_left1 : Node3D = chunk1.instantiate()
+	add_child(chunk_left1)
+	chunk_left1.position = Vector3(0-CHUNK_SIZE,0,0)
 	
-	
-	
-	print("generate initial world")
-	
+	MaxPlacedChunks = 1
+	MinPlacedChunks = 1
+	pass
+
+func on_move_place_chunk(player_position):
+	print(str((player_position.x + RENDER_DISTANCE)/CHUNK_SIZE))
+	if (((player_position.x + RENDER_DISTANCE)/CHUNK_SIZE)>MaxPlacedChunks):
+		var chunk_right : Node3D = chunk1.instantiate()
+		add_child(chunk_right)
+		chunk_right.position = Vector3(0+(CHUNK_SIZE*MaxPlacedChunks),0,0)
+		print(CHUNK_SIZE*MaxPlacedChunks)
+		MaxPlacedChunks = MaxPlacedChunks + 1
+		print("place new chunk right")
+		
+	if (((player_position.x - RENDER_DISTANCE)/CHUNK_SIZE)<MinPlacedChunks):
+		var chunk_left : Node3D = chunk1.instantiate()
+		add_child(chunk_left)
+		chunk_left.position = Vector3(0-(CHUNK_SIZE*abs(MinPlacedChunks)),0,0)
+		print(CHUNK_SIZE*MinPlacedChunks)
+		MinPlacedChunks = MinPlacedChunks - 1
+		print("place new chunk left")
+		
 	pass
