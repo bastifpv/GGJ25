8d20ec1800b493e54a5fbdd913600be4c893ddb5
rellig <rellig@minad.de>
2025-01-25 15:39:33 +0100

    fix ui overlay

--- scripts/ui_needle_oxy.gd
+++ scripts/ui_needle_oxy.gd
@@ -3,6 +3,8 @@ var oxy = 1000
 var pop = 2
 var relative_oxy
 
+@export var player: Node3D
+@export var dome: Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
@@ -28,8 +30,8 @@ func _process(delta: float) -> void:
 	#print($"../../Player".global_position.x)
 	#print ("Y: ")
 	#print($"../../Player".global_position.y)
-	var rel_y = $"../../..".global_position.y - $"../../../../../Dome".global_position.y
-	var rel_x = $"../../..".global_position.x - $"../../../../../Dome".global_position.x
+	var rel_y = player.global_position.y - dome.global_position.y
+	var rel_x = player.global_position.x - dome.global_position.x
 	var angle_to_dome = 180 - (tan(rel_y / rel_x)*180/3.1415926)
 	$home/Arrow_home.rotation_degrees = angle_to_dome
 
